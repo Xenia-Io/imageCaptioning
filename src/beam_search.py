72ca6506e9b44613f40d1c99ea8aa4dba64e41b1
Deepika Anantha Padmanaban <32296513+deepikaaap@users.noreply.github.com>
2019-05-09 19:17:48 +0200

    bug fix

--- src/beam_search.py
+++ src/beam_search.py
@@ -9,15 +9,15 @@ def beam_search(decoder, input_features, vocabulary, beam_size):
   batch_size = input_features.size(0)
   num_of_features = input_features.size(1)
   dim_of_features = input_features.size(2)
-  vocab_size = len(vocabulary)
+  vocab_size = vocabulary.count
   max_cap_len = 13
 
   # expanding input features by replicating beam_size times
   input_features = input_features.expand(beam_size,num_of_features,dim_of_features)
 
   # topk words in the beginning are assigned as start tag
-  prev_topk_words = torch.LongTensor([[vocabulary['<start>']]] * beam_size).cuda()
-  prev_topk_wordembeds = decoder.word_embed(Variable(prev_topk_words)).squeeze(1)
+  prev_topk_words = torch.LongTensor([[vocabulary.word2id['<start>']]] * beam_size).cuda()
+  prev_topk_wordembeds = decoder.module.word_embed(Variable(prev_topk_words)).squeeze(1)
 
   # keeping track of best k sequences at any time step
   squences = prev_topk_words
@@ -28,7 +28,7 @@ def beam_search(decoder, input_features, vocabulary, beam_size):
   # keeping track of alpha (attention weights) corresponding to best k sequences
   alpha_squences = torch.ones(beam_size,1,num_of_features).cuda()
 
-  hidden_dim = decoder.hidden_size
+  hidden_dim = decoder.module.hidden_size
   h_curr = Variable(torch.zeros(beam_size, hidden_dim).cuda())
   c_curr = Variable(torch.zeros(beam_size, hidden_dim).cuda())
 
@@ -42,12 +42,12 @@ def beam_search(decoder, input_features, vocabulary, beam_size):
 
   for t in range(max_cap_len):
     if(t!=0):
-      prev_topk_wordembeds = decoder.word_embed(Variable(prev_topk_words)).squeeze(1)
-      context, alpha = decoder.predict_attention(input_features.cuda(), h_curr)
+      prev_topk_wordembeds = decoder.module.word_embed(Variable(prev_topk_words)).squeeze(1)
+      alpha, context = decoder.module.predict_attention(input_features.cuda(), h_curr)
 
     decoder_input = torch.cat([context, prev_topk_wordembeds], dim=1)
-    h_curr, c_curr = decoder.lstm_cell(decoder_input, (h_curr, c_curr))
-    scores = decoder.deep_output(h_curr)
+    h_curr, c_curr = decoder.module.lstm_cell(decoder_input, (h_curr, c_curr))
+    scores = decoder.module.deep_output(h_curr)
     scores = torch.nn.functional.log_softmax(scores, dim=1)
 
     # update the scores by adding current step scores
@@ -81,7 +81,7 @@ def beam_search(decoder, input_features, vocabulary, beam_size):
 
     # removing completed sequences and related columns in features and other variables
     squences = squences[incomp_captions] 
-    alpha_squences = seqs_alpha[incomp_captions]
+    alpha_squences = alpha_squences[incomp_captions]
     prev_words = topk_words / vocab_size
     curr_words = topk_words % vocab_size
     h_curr = h_curr[prev_words[incomp_captions]]
@@ -100,12 +100,12 @@ def beam_search(decoder, input_features, vocabulary, beam_size):
 def findk_wordseq_alphas(topk_words, squences, alpha_squences, vocab_size, alpha):
   prev_words = topk_words/vocab_size
   curr_words = topk_words%vocab_size
-  squences = torch.cat([squences[prev_words], curr_words.unsqueeze(1).float()], dim=1)
+  squences = torch.cat([squences[prev_words], curr_words.unsqueeze(1).long()], dim=1)
   alpha_squences = torch.cat([alpha_squences[prev_words], alpha[prev_words].unsqueeze(1).float()], dim=1)
   return squences, alpha_squences
 
 def find_sequences(topk_words, vocabulary):
-  curr_words = topk_words%len(vocabulary)
-  incomp_captions = [ind for ind, curr_word in enumerate(curr_words) if curr_word != vocabulary("<end>")]
+  curr_words = topk_words%(vocabulary.count)
+  incomp_captions = [ind for ind, curr_word in enumerate(curr_words) if curr_word != vocabulary.word2id["<end>"]]
   comp_captions = list(set(range(len(curr_words))) - set(incomp_captions))
   return incomp_captions, comp_captions
