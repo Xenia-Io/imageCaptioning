19f425b5eb285b35053ca1a2689ea6a7a61f5ab9
Deepika Anantha Padmanaban <32296513+deepikaaap@users.noreply.github.com>
2019-05-17 11:46:38 +0200

    updated with ResNet class

--- src/encoder.py
+++ src/encoder.py
@@ -12,8 +12,22 @@ class VGGNetEncoder(nn.Module):
         # As the paper mentions the use of the output from a lower convolutional layer
         self.net = nn.Sequential(*list(self.net.features.children())[:-1])
 
-
     def forward(self, x):
         feature = self.net(x)
  
+        return feature
+
+
+class ResNetEncoder(nn.Module):
+    def __init__(self):
+        super(ResNetEncoder, self).__init__()
+        # Loading a pretrained ResNet model
+        resnet = models.resnet152(pretrained=True)
+        # Removing the last fc layer as per the paper
+        self.extracted_net = nn.Sequential(*(list(resnet.children())[:-2]))
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((14, 14))
+
+    def forward(self, x):
+        feature = self.extracted_net(x)
+        feature = self.adaptive_pool(feature)
         return feature
